690c53770ec8e6bf7f426e5f6580bdf994cd9809
artemijr <arrtemijr@gmail.com>
2024-01-12 17:31:33 +0300

    registration works normal, login needs to be refactored.

--- src/main/kotlin/org/bogdans/config/SecurityConfig.kt
+++ src/main/kotlin/org/bogdans/config/SecurityConfig.kt
@@ -1,40 +1,95 @@
 package org.bogdans.config
 
-import org.bogdans.security.CustomCorsFilter
-import org.bogdans.security.CustomJwtAuthFilter
+import org.bogdans.security.JwtRequestFilter
+import org.bogdans.util.JwtTokenUtil
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.access.channel.ChannelProcessingFilter
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(private val jwtAuthFilter: CustomJwtAuthFilter) {
+class SecurityConfig {
+
+    @Autowired
+    private lateinit var authenticationConfiguration: AuthenticationConfiguration
+
+//    @Autowired
+//    fun configureGlobal(auth: AuthenticationManagerBuilder, passwordEncoder: PasswordEncoder, userDetailsService: UserDetailsService) {
+//        val daoAuthenticationProvider = DaoAuthenticationProvider()
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService)
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder)
+//        auth.authenticationProvider(daoAuthenticationProvider)
+//    }
+    @Autowired
+    private lateinit var jwtTokenUtil: JwtTokenUtil
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
+
+
+
+    @Bean
+    fun jwtRequestFilter(): JwtRequestFilter {
+        return JwtRequestFilter(jwtTokenUtil, userDetailsService)
+    }
+
+    @Bean
+    fun authenticationManager(): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
 
         http
-            .authorizeHttpRequests { auth ->
-                auth
-                    .requestMatchers("/api/v1/auth/**").permitAll()
+            .addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter::class.java)
+            .cors { cors -> cors.configurationSource(corsConfigurationSource()) }
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { request ->
+                request
+                    .requestMatchers("/api/register").permitAll()
+                    .requestMatchers("/api/authenticate").permitAll()
                     .anyRequest().authenticated()
             }
-            .sessionManagement { sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
-            .addFilterBefore(CustomCorsFilter(), ChannelProcessingFilter::class.java)
-            .logout { logout ->
-                logout.logoutUrl("/api/v1/auth/logout")
-                    .logoutSuccessHandler { request, response, authentication ->
-                        SecurityContextHolder.clearContext()
-                    }.deleteCookies()
-            }
+
+            .formLogin { formLogin -> formLogin.defaultSuccessUrl("/api/success") }
+            .logout { logout -> logout.logoutSuccessUrl("/api/logoutSuccess") }
 
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000") // or use listOf("*") for allowing all origins
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
--- src/main/kotlin/org/bogdans/controller/UserController.kt
+++ src/main/kotlin/org/bogdans/controller/UserController.kt
@@ -1,17 +1,44 @@
 package org.bogdans.controller
 
-import org.bogdans.model.User
+import org.bogdans.dto.UserRegistrationDto
 import org.bogdans.service.UserService
+import org.bogdans.util.JwtTokenUtil
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/users")
-class UserController(private val userService: UserService) {
+@RequestMapping("/api")
+class UserController(
+    private val userService: UserService,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtTokenUtil: JwtTokenUtil,
+    private val userDetailsService: UserDetailsService
 
-    @GetMapping("/{username}")
-    fun getUserByUsername(@PathVariable username: String): ResponseEntity<User> {
-        val user = userService.findUserByUsername(username)
-        return if (user != null) ResponseEntity.ok(user) else ResponseEntity.notFound().build()
+) {
+
+    @PostMapping("/register")
+    fun registerUser(@RequestBody userDto: UserRegistrationDto): ResponseEntity<Any> {
+        userService.registerUser(userDto)
+        return ResponseEntity.ok().build()
     }
+
+    @PostMapping("/authenticate")
+    fun createAuthenticationToken(@RequestBody userDto: UserRegistrationDto): ResponseEntity<Any> {
+        try {
+            authenticationManager.authenticate(UsernamePasswordAuthenticationToken(userDto.username, userDto.password))
+            val userDetails = userDetailsService.loadUserByUsername(userDto.username)
+            val token = jwtTokenUtil.generateToken(userDetails)
+            return ResponseEntity.ok(mapOf("token" to token))
+        } catch (ex: Exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        }
+    }
+
+
+
 }
