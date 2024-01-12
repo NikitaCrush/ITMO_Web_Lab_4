1a2b01f6001abb5d3f4760e1732708b06f3b2b64
artemijr <arrtemijr@gmail.com>
2024-01-12 17:32:17 +0300

    changed auth.js a bit to work with server

--- client/src/suppliers/auth.js
+++ client/src/suppliers/auth.js
@@ -1,61 +1,58 @@
 import axios from 'axios';
-import {useDispatch} from "react-redux";
-import {loginAuth,} from "../redux/actions/pointsActions";
+
 
 export function useAuth() {
-    const dispatch = useDispatch();
     const register = async (login, password) => {
         try {
             const response = await axios.post(
-                'http://localhost:8080/register',
-                {
-                    username: login,
-                    password: password,
-                }
+                'http://localhost:8080/api/register',
+                { username: login, password: password },
+                { headers: { 'Content-Type': 'application/json' }}
             );
             console.log('Registration successful', response.data);
+            return response.data;
         } catch (error) {
-            console.error('Registration failed.', error);
-            throw error;
+            console.error('Registration failed:', error.response.data);
+            throw new Error(error.response.data);
         }
     };
-
 
     const login = async (login, password) => {
         try {
             const response = await axios.post(
-                'http://localhost:8080/login',
-                new URLSearchParams({
-                    username: login,
-                    password: password,
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                    },
-                    withCredentials: true,
-                }
+                'http://localhost:8080/api/authenticate',
+                { username: login, password: password },
+                { headers: { 'Content-Type': 'application/json' }}
             );
-            console.log('Login successful', response.data);
-            dispatch(loginAuth());
+            if (response.data && response.data.token) {
+                // Store the JWT token in local storage or context
+                localStorage.setItem("jwtToken", response.data.token);
+                console.log('Login successful', response.data);
+                return response.data;
+            }
         } catch (error) {
-            console.error('Login failed.', error);
-            throw error;
-        }
-    };
-    const logout = async () => {
-        try {
-            await axios.post('http://localhost:8080/logout', {
-                withCredentials: true,
-            });
-            console.log("Logout done")
-        } catch (error) {
-            console.error('Failed to logout:', error);
+            console.error('Login failed:', error.response.data);
+            throw new Error(error.response.data);
         }
     };
 
-    return {
-        register,
-        login, logout,
+
+    const logout = async () => {
+        try {
+            const response = await axios.post(
+                'http://localhost:8080/api/logout',
+                null,
+                { withCredentials: true }
+            );
+            localStorage.removeItem('jwtToken'); // Remove the JWT token from local storage
+
+            console.log("Logout successful", response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Failed to logout:', error.response.data);
+            throw new Error(error.response.data);
+        }
     };
+
+    return { register, login, logout };
 }
